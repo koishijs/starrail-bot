f26bf5dd32974d76e95e1ecc54f57382823dddda
Lipraty <sis1943@outlook.com>
2023-05-23 23:13:07 +0800

    refa(core): refactor project

---
+++ packages/starrail/src/downloader.ts
@@ -0,0 +1,13 @@
+import { Context } from "koishi";
+
+class StarRailDownloader {
+    constructor(ctx: Context) { }
+}
+
+namespace StarRailDownloader {
+    export const DataDir = 'data/starrail'
+    export const TempDir = 'temp/starrail'
+    export const Register = ''
+}
+
+export default StarRailDownloader
