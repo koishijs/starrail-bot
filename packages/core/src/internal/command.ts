faba18fe42e41ed0ccf737c4213788d569b09a4d
Lipraty <sis1943@outlook.com>
2023-05-28 21:18:08 +0800

    chore(core): move all commnad function

--- packages/core/src/internal/command.ts
+++ packages/core/src/internal/command.ts
@@ -1,9 +1,9 @@
-import { Context } from "koishi";
-import HonkaiStarRail from "..";
+import { Command, Context, Schema } from "koishi";
 
-export default class StarRailCommander {
-    static using = ['starrail']
-    constructor(ctx: Context, private config: HonkaiStarRail.Config) {
+type CommandType = 'subset' | 'derive'
+
+class StarRailCommander {
+    constructor(private ctx: Context, private config: StarRailCommander.Config) {
         //#region commands
         ctx.starrail.subcommand('uid <uid>')
             .option('default', '-d')
@@ -41,4 +41,30 @@ export default class StarRailCommander {
             })
         //#endregion
     }
+
+    private defineCommand(command: string, type: CommandType, ...args: any[]): Command {
+        const def = type === 'subset' ? `sr.${command}` : `sr/${command}`
+        const desc = typeof args[0] === 'string' ? args.shift() as string : ''
+        const config = args[0] as Command.Config || {}
+        return this.ctx.command(def, desc, config)
+    }
+
+    subcommand<D extends string>(def: D, config?: Command.Config): Command
+    subcommand<D extends string>(def: D, desc: string, config?: Command.Config): Command
+    public subcommand(def: string, ...args: any[]) {
+        return this.defineCommand(def, 'subset', ...args)
+    }
+
+    dercommand<D extends string>(def: D, config?: Command.Config): Command
+    dercommand<D extends string>(def: D, desc: string, config?: Command.Config): Command
+    public dercommand(def: string, ...args: any[]) {
+        return this.defineCommand(def, 'derive', ...args)
+    }
 }
+
+namespace StarRailCommander {
+    export interface Config { }
+    export const Config: Schema<Config> = Schema.object({}).description('命令设置')
+}
+
+export default StarRailCommander
