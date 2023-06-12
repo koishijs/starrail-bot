5bc1ae19596dce702ecf26d50beee8a0943c0bd3
Lipraty <sis1943@outlook.com>
2023-06-13 04:35:23 +0800

    feat(core): change `sr.uid` command and update locale

--- packages/core/src/internal/command.ts
+++ packages/core/src/internal/command.ts
@@ -1,4 +1,5 @@
-import { Command, Context, Schema, Session } from "koishi";
+import { Command, Context, Schema } from "koishi";
+import { IsDot } from "../utils/IsDot";
 
 type CommandType = 'subset' | 'derive'
 
@@ -6,54 +7,72 @@ class StarRailCommander {
   static using = ['starrail']
   constructor(private ctx: Context, private config: StarRailCommander.Config) {
     //#region commands
-    ctx.starrail.subcommand('uid <uid>')
+    ctx.command('sr.uid [uid:string]')
       .option('default', '-d')
+      .option('device', '-D')
       .option('remove', '-r')
-      .userFields(['sr_uid'])
+      .option('user', '-u')
+      .userFields(['sr_uid', 'id'])
       .action(async ({ session, options }, uid) => {
+        const frist = !session.user.sr_uid
+        const uids = await ctx.starrail.getUid(session.user.id)
+        const findAll = (await ctx.database.get('star_rail', { uid }, ['bid'])).map(u => u.bid)
+
+        if (findAll.length > 0 && !findAll.includes(session.user.id) && Object.keys(options).length <= 0) return session.text('.fail-binded')
+        if (options.default && !uid) return
+
+        if (!uid)
+          if (Object.keys(options).length > 0) uid = session.user.sr_uid
+          else
+            if (uids.length <= 0) {
+              return session.text('.list-none')
+            } else
+              return `<message>
+              <p>${session.text('.list')}</p>
+              ${uids
+                  .sort(uid => uid === session.user.sr_uid ? -1 : 1)
+                  .map((uid, i) => `<p>    ${i + 1}. ${uid + (i === 0
+                    ? session.text('.list-def')
+                    : '')}</p>`)
+                  .join('')
+                }
+              </message>`
+
+        // Option filter
         if (options.default) {
+          if (uid === session.user.sr_uid) return session.text('.fail-isdef', [uid])
           session.user.sr_uid = uid
-          return session.text('.def', [uid])
+          return session.text('.bind-def', [uid])
+        } else if (options.remove && options.user) {
+          // TODO
+          return
         } else if (options.remove) {
+          if (!uids.includes(uid)) return session.text('.fail-remove')
           if (session.user.sr_uid === uid) {
-            await session.send(session.text('.remove-def', [uid]))
+            await session.send(session.text('.warn-rmdef', [uid]))
             const dot = await session.prompt(1500)
-            if (/\.|。/.test(dot)) {
-              return session.text('.removed-def')
-            }
+            if (dot && !IsDot(dot)) return session.text('.fail-dot')
             return session.text('.timeout')
           }
-          return session.text('.removed', [uid])
+          await ctx.database.remove('star_rail', { uid })
+          return session.text('.bind-removed')
+        } else if (options.user) {
+          // TODO
+          return
+        } else if (options.device) {
+          // TODO
+          return
         }
-        if (session.user.sr_uid) {
-          //
-        } else {
-          session.user.sr_uid = uid
-          //
-          return session.text('.binded') + session.text('.frist', [uid])
-        }
-
+        console.log(frist, uid)
+        await ctx.starrail.setUid(session.user.id, uid, frist)
+        return `<i18n path=".bind-saved">${[uid]}</i18n>${frist ? session.text('.bind-frist') : ''}`
       })
-    ctx.starrail.subcommand('cookie [uid]')
-      .option('bind', '-b')
+    // private only
+    ctx.private().command('sr.cookie [cookie]')
+      .option('uid', '-u')
       .option('remove', '-r')
       .action(async () => {
 
-      })
-    ctx.starrail.subcommand('bind <uid:string>')
-      .alias('星铁绑定')
-      .action(async ({ session }, uid) => {
-        if (!uid) {
-          session.send('请输入uid')
-          uid = await session.prompt()
-        }
-        if (!uid) return
-        const session_user: Session<"id"> = session as Session<"id">
-        const uid_base = (await this.ctx.starrail.getUid(session_user.user.id))[0]?.uid
-        let user_enable = false
-        if (uid_base) user_enable = true
-        await this.ctx.starrail.setUid(session_user.user.id, uid, user_enable)
-        return '绑定成功'
       })
     //#endregion
   }
@@ -65,12 +84,18 @@ class StarRailCommander {
     return this.ctx.command(def, desc, config)
   }
 
+  /**
+   * @deprecated does not work with `hmr`, you can use `ctx.command('sr.[your command]')`
+   */
   subcommand<D extends string>(def: D, config?: Command.Config): Command
   subcommand<D extends string>(def: D, desc: string, config?: Command.Config): Command
   public subcommand(def: string, ...args: any[]) {
     return this.defineCommand(def, 'subset', ...args)
   }
 
+  /**
+   * @deprecated does not work with `hmr`, you can use `ctx.command('sr [your command]')`
+   */
   dercommand<D extends string>(def: D, config?: Command.Config): Command
   dercommand<D extends string>(def: D, desc: string, config?: Command.Config): Command
   public dercommand(def: string, ...args: any[]) {
