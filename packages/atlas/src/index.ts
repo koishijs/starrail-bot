df7744284cb207d07629cc5ec06a308a2287b1c7
initialencounter <2911583893@qq.com>
2023-11-26 09:57:22 +0800

    fix: guide for role

--- packages/atlas/src/index.ts
+++ packages/atlas/src/index.ts
@@ -2,7 +2,7 @@ import { Context, Schema, Session, h, Dict } from 'koishi'
 export const name = 'starrail-atlas'
 import { resolve } from "path";
 import { pathToFileURL } from "url";
-import fs from 'fs';
+import fs, { readFileSync } from 'fs';
 class StarRailAtlas {
   path_data: Dict
   path_dict: Dict
@@ -10,11 +10,11 @@ class StarRailAtlas {
   constructor(private ctx: Context, private config: StarRailAtlas.Config) {
     ctx.i18n.define('zh', require('./locales/zh'))
     ctx.on('ready', async () => {
-      this.path_data = require(resolve(__dirname,'path.json'))
+      this.path_data = require(resolve(__dirname, 'path.json'))
       this.path_dict = {}
-      let keys = ['relic', 'role', 'lightcone', 'material for role','enemy'];
+      let keys = ['relic', 'role', 'lightcone', 'guide for role', 'enemy'];
       let keyMapping = {
-        'material for role': '材料'
+        'guide for role': '攻略'
       };
       this.name_list = [];
       for (let key of keys) {
@@ -51,23 +51,7 @@ class StarRailAtlas {
   }
 }
 namespace StarRailAtlas {
-  export const usage = `
-> 如未启用在线引擎，请在前往 <a style="color:blue" href="https://gitee.com/Nwflower/star-rail-atlas/tree/master/role">Nwflower/star-rail-atlas</a> 中下载资源文件并解压<br>
-并填写资源文件的路径<br>
-本图鉴目前仅供学习交流使用，素材版权为米哈游所有。<br>
-对于部署者行为及所产生的任何纠纷， Koishi 及 koishi-plugin-star-rail-atlas 概不负责。
-- 角色图鉴
-    - #三月七
-- 光锥图鉴
-    - #此时恰好
-- 角色材料
-    - #三月七材料
-- 更新图鉴
-    - update
-## 问题反馈
-* QQ群：399899914<br>
-* 小伙伴如果遇到问题或者有新的想法，欢迎到[这里](https://github.com/initialencounter/mykoishi/issues)反馈哦~
-`;
+  export const usage = `${readFileSync(resolve(__dirname, '../readme.md')).toString('utf-8').split('更新日志')[0]}`
   export interface Config {
     prefix: string
     src_path: string
