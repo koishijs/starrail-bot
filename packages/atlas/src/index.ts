56126ab8d1b56d0bf92ce81f39f109e4d3ff9b0b
initialencounter <2911583893@qq.com>
2024-04-13 16:58:25 +0800

    refactor(atlas): Adapt to the latest atlas

--- packages/atlas/src/index.ts
+++ packages/atlas/src/index.ts
@@ -1,53 +1,102 @@
 import { Context, Schema, Session, h, Dict } from 'koishi'
 export const name = 'starrail-atlas'
-import { resolve } from "path";
-import { pathToFileURL } from "url";
+import { resolve } from 'path';
+import { pathToFileURL } from 'url';
 import fs, { readFileSync } from 'fs';
 class StarRailAtlas {
-  path_data: Dict
-  path_dict: Dict
-  name_list: string[]
+  othername: Dict = {}
+  path_dict: Dict = {}
+  relic_dict: Dict = {}
+  role_dict: Dict = {}
+  lightcone_dict: Dict = {}
+  enemy_dict: Dict = {}
+  alias: Dict = {}
   constructor(private ctx: Context, private config: StarRailAtlas.Config) {
     ctx.i18n.define('zh', require('./locales/zh'))
     ctx.on('ready', async () => {
-      this.path_data = require(resolve(__dirname, 'path.json'))
-      this.path_dict = {}
-      let keys = ['relic', 'role', 'lightcone', 'guide for role', 'enemy'];
-      let keyMapping = {
-        'guide for role': '攻略'
-      };
-      this.name_list = [];
-      for (let key of keys) {
-        let list = Object.keys(this.path_data[key]);
-        list.map(i => {
-          this.path_dict[i + (keyMapping[key] || '')] = this.path_data[key][i];
-        });
-        Array.prototype.push.apply(this.name_list, list);
+      this.othername = require(resolve(__dirname, 'othername.json'))
+      this.path_dict = require(resolve(__dirname, 'path.json'))
+      // relic
+      for (let id of Object.keys(this.othername['relic'])) {
+        for (let othername of this.othername['relic'][id]) {
+          this.relic_dict[othername] = id
+        }
+        this.relic_dict[id] = id
+      }
+
+      // role
+      for (let id of Object.keys(this.othername['role'])) {
+        for (let othername of this.othername['role'][id]) {
+          this.role_dict[othername] = id
+        }
+        this.role_dict[id] = id
+      }
+
+      // lightcone
+      for (let id of Object.keys(this.othername['lightcone'])) {
+        for (let othername of this.othername['lightcone'][id]) {
+          this.lightcone_dict[othername] = id
+        }
+        this.lightcone_dict[id] = id
+      }
+
+      // enemy
+      this.enemy_dict = this.path_dict['enemy']
+
+      for (let alias_class of Object.keys(config.alias)) {
+        for (let alias of config.alias[alias_class]) {
+          this.alias[alias] = alias_class
+        }
       }
     })
     ctx.middleware((session, next) => {
-      const path = this.findpath(session.content);
-      if (path == '') return next();
+      const target = this.getTarget(session.content);
+      if (target.length < 2) return next();
+      let path: string
+      switch (target[0]) {
+        case 'role':
+          path = '/role/' + this.role_dict[target[1]] + '.png'
+          break
+        case 'relic':
+          path = '/relic/' + this.relic_dict[target[1]] + '.png'
+          break
+        case 'lightcone':
+          path = '/lightcone/' + this.lightcone_dict[target[1]] + '.png'
+          break
+        case 'enemy':
+          path = this.enemy_dict[target[1]]
+          break
+        case 'material for role':
+          path = '/guide for role/' + this.enemy_dict[target[1]] + '.png'
+          break
+      }
       let img_url: string
       if (config.engine) {
         img_url = this.config.repo + path
       } else {
         img_url = pathToFileURL(resolve(this.config.src_path + path)).href
       }
+      console.log(img_url)
       return h.image(img_url);
     })
     ctx.command('sr.atlas', '更新图鉴索引').alias('更新图鉴索引').action(({ session }) => this.update(session))
   }
   async update(session: Session) {
     const res = await this.ctx.http.get('https://gitee.com/Nwflower/star-rail-atlas/raw/master/path.json', { responseType: 'arraybuffer' })
-    fs.writeFileSync('./node_modules/koishi-plugin-starrail-atlas/lib/path.json', res)
+    fs.writeFileSync('./node_modules/koishi-plugin-starrail-atlas/lib/path.json', Buffer.from(res))
+    const res2 = await this.ctx.http.get('https://gitee.com/Nwflower/star-rail-atlas/raw/master/othername.json', { responseType: 'arraybuffer' })
+    fs.writeFileSync('./node_modules/koishi-plugin-starrail-atlas/lib/othername.json', Buffer.from(res2))
     return session.text('commands.update.messages.success')
   }
-  findpath(cmd: string): string {
-    if (!(cmd.startsWith(this.config.prefix))) return ""
-    const name = cmd.replace(this.config.prefix, '')
-    const path = this.path_dict[name]
-    return path ? path : ""
+  getTarget(cmd: string): string[] {
+    if (!(cmd.startsWith(this.ctx.config.prefix))) return []
+    cmd = cmd.replace(this.ctx.config.prefix, '')
+    if (!cmd) return []
+    let [alias, name] = cmd.split(' ')
+
+    let alias_class = this.alias[alias]
+    if (!alias_class) return []
+    return [alias_class, name]
   }
 }
 namespace StarRailAtlas {
@@ -57,13 +106,29 @@ namespace StarRailAtlas {
     src_path: string
     engine: boolean
     repo: string
+    alias: {
+      role: string[]
+      enemy: string[]
+      lightcone: string[]
+      'material for role': string[]
+      relic: string[]
+    }
   }
+  export const Alias = Schema.object({
+    role: Schema.array(String).default(['角色']).description('角色'),
+    enemy: Schema.array(String).default(['怪', 'boss']).description('敌人'),
+    lightcone: Schema.array(String).default(['光锥']).description('光锥'),
+    'material for role': Schema.array(String).default(['角色材料']).description('角色材料'),
+    relic: Schema.array(String).default(['遗器']).description('遗器'),
+  })
+
   export const Config: Schema<Config> =
     Schema.object({
       prefix: Schema.string().default('#').description('匹配命令的前缀字符'),
       engine: Schema.boolean().default(true).description('是否使用在线引擎'),
       src_path: Schema.string().default('star-rail-atlas').description('资源文件的路径'),
       repo: Schema.string().default('https://gitee.com/Nwflower/star-rail-atlas/raw/master').description('gitee在线资源的地址'),
+      alias: Alias,
     }).description('进阶设置')
 
 }
